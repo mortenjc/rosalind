5a705755cc922bffd238e1795b9b2dc03280f5ba
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-06-18 00:49:46 +0200

    SSEQ in the bank

---
+++ python/SSEQ/sseq.py
@@ -0,0 +1,36 @@
+import sys, math, re
+sys.path.append('../common')
+import toolbox as tb
+import files as f
+
+# https://rosalind.info/problems/sseq
+
+def search(patt, string):
+    res = patt.match(string)
+    if res:
+        return True
+    return False
+
+filename = f.filefromargv(sys.argv)
+n, names, strings = f.readfasta(filename)
+#lines = f.readlines(filename)
+
+assert n == 2
+
+string = strings[0]
+patt = strings[1]
+np = len(patt)
+a = [f'({i})' for i in list(patt)]
+repatt = '.*'.join(a)
+print(string)
+print(repatt)
+
+pattern = re.compile(repatt)
+for i in range(len(string)):
+    #print(i)
+    res = pattern.match(string[i:])
+    if res:
+        r = map(str, [1+i+res.start(n) for n in range(1,np+1)])
+        print(r)
+        print(' '.join(r))
+        break
