7096e1e336217aba854471da4a4c52a50311ec36
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-05-05 23:45:56 +0200

    need done

--- python/NEED/need.py
+++ python/NEED/need.py
@@ -8,20 +8,32 @@ import strings as s
 #
 # #
 #
+from Bio import Entrez
+
+def getfasta(name):
+    Entrez.email = "mortenjc@jcapd.com"
+    handle = Entrez.efetch(db="nucleotide", id=name, rettype="fasta")
+    record = handle.read()
+    assert record[0] == '>'
+    tmp = record.split('\n')
+    return tmp[0], ''.join(tmp[1:])
 
 
 filename = f.filefromargv(sys.argv)
 #n, names, strings = f.readfasta(lines)
 #print(n, names, strings)
 lines = f.readlines(filename)
-
 assert len(lines) == 1
+assert len(lines[0].split()) == 2
+n1, n2 = lines[0].split()
 
-id = f'"{lines[0]}"'
+f1h, f1n = getfasta(n1)
+print(f1n)
 
-from Bio import Entrez
-Entrez.email = "mortenjc@jcapd.com"
-handle = Entrez.efetch(db="nucleotide", id=[id], rettype="fasta")
-records = handle.read().split('\n')
+print()
 
-print(records)
+f2h, f2n = getfasta(n2)
+print(f2n)
+
+# Then use this page (DNA option), more options set gap penalty
+# https://www.ebi.ac.uk/jdispatcher/psa/emboss_needle
