271a702232733b85d34e3f51a9ed8ecc40293566
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-05-07 23:17:48 +0200

    ba1c

---
+++ python/ba1a/ba1a.py
@@ -0,0 +1,32 @@
+import sys, math, re
+sys.path.append('../common')
+import toolbox as tb
+import files as f
+import strings as s
+
+# https://rosalind.info/problems/ba1a
+
+#
+# #
+#
+
+def match(s1, pat):
+    count = 0
+    ls1 = len(s1)
+    l = len(pat)
+    assert ls1 >= l
+
+    for i in range(ls1 - l + 1):
+        if s1[i:i+l] == pat:
+            count += 1
+    return count
+
+
+filename = f.filefromargv(sys.argv)
+lines = f.readlines(filename)
+#n, names, seqs, qual = f.readfastq(lines[1:])
+
+s1 = lines[0]
+pat = lines[1]
+
+print(match(s1, pat))
