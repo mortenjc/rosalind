dc551630bd2fe492777291b0ea2ce736c9b60480
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-05-08 19:09:27 +0200

    BA1F done after some optimisation

---
+++ python/BA1F/ba1f.py
@@ -0,0 +1,33 @@
+import sys, math, re
+sys.path.append('../common')
+#import toolbox as tb
+import files as f
+#import strings as s
+from collections import defaultdict
+
+# https://rosalind.info/problems/ba1e
+
+#
+# #
+#
+
+
+filename = f.filefromargv(sys.argv)
+lines = f.readlines(filename)
+#n, names, seqs, qual = f.readfastq(lines[1:])
+
+seq = lines[0]
+res = [0]
+d = defaultdict(int)
+for i in range(len(seq)):
+    ch = seq[i]
+    d[ch] += 1
+    res.append(d['G'] - d['C'])
+
+minval = min(res)
+
+res2 = []
+for i in range(len(seq)):
+    if res[i] == minval:
+        res2.append(str(i))
+print(' '.join(res2))
